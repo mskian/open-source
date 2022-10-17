848506c929f5c914a856f31eb387dfbb3ac6a131
mskian <10300271+mskian@users.noreply.github.com>
2022-10-17 13:51:42 +0530

    Testing CF og image

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -6,7 +6,6 @@ class ElementHandler {
     element.append(ogtag, { html: true })
   }
 }
-const rewriter = new HTMLRewriter().on("head", new ElementHandler())
 
 export async function onRequest(context) {
   const {
@@ -18,21 +17,18 @@ export async function onRequest(context) {
     data,
   } = context
 
-   // get query strings provided with request and path name accessing the page
    const { searchParams, pathname } = new URL(request.url)
 
-  if (pathname == '/diwali/index.html' || pathname == '/diwali') {
+   let res = await next()
 
-  let res = await next()
+   if (!(pathname === '/diwali' || pathname === '/diwali/index.html')) {
+      return res
+  }
 
-  // querystring I'm looking for to create dynamic images, this is an optional thing depending on your case
   name = searchParams.get("name")
-
-  // these are the metatags we want to inject into the site
   ogtag = `
     <meta property="og:url" content="${request.url}" />
     <meta property="og:image" content="https://images.weserv.nl/?url=https://img.sanweb.info/dw/dw?name=${name}" />
   `
-  return rewriter.transform(res)
-  }
+  return new HTMLRewriter().on("head", new ElementHandler())
 }
